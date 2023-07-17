16137f076f60def2fa292853c5e93dfd96887510
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-07-17 16:13:28 -0400

    commit tree c64edb577cf85ab9ee9c349efe543d8e576815fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :spices, only:[:index,:create,:update, :destory]
 end
+# This code defines the RESTful routes for the spices resource, restricting them to only the specified actions: index, create, update, and destroy.
+
